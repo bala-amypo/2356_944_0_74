da88a3466e04a278187cfa843cf1cb04eb32dba3
bala-amypo <bala@amypo.in>
2025-12-17 09:34:40 +0000

    User commit

--- src/main/java/com/example/demo/newservice/newserviceimpl/NewServiceImpl.java
+++ src/main/java/com/example/demo/newservice/newserviceimpl/NewServiceImpl.java
@@ -18,7 +18,7 @@ public class NewfileServiceImpl implements NewfileService {
 
     @Override
     public NewfileEntity getidval(Long id) {
-        
+        return mp.get(id)
     }
 
     @Override
